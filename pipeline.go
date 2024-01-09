342500857fb0bbdb9d076e73e6ae2fc09aa430f7
Daniel Kolomeets <kolomeetz.dany@yandex.ru>
2024-01-09 21:35:22 +0300

    Log pipeline stages

--- pipeline.go
+++ pipeline.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"log"
 	"time"
 )
 
@@ -34,16 +35,20 @@ func filterNegativeValues(done <-chan bool, data <-chan int) <-chan int {
 	filtered := make(chan int)
 
 	go func() {
+		log.Println("filterNegativeValues: start filtering")
 		for val := range data {
 			// skip negative values
 			if val < 0 {
+				log.Printf("filterNegativeValues: skip value %v\n", val)
 				continue
 			}
 
 			select {
 			case <-done:
+				log.Println("filterNegativeValues: stop filtering")
 				return
 			case filtered <- val:
+				log.Printf("filterNegativeValues: passed value %v\n", val)
 			}
 		}
 	}()
@@ -55,16 +60,20 @@ func filterSpecificValues(done <-chan bool, data <-chan int) <-chan int {
 	filtered := make(chan int)
 
 	go func() {
+		log.Println("filterSpecificValues: start filtering")
 		for val := range data {
 			// skip 0 and not divisible by three
 			if val == 0 || (val%3 != 0) {
+				log.Printf("filterSpecificValues: skip value %v\n", val)
 				continue
 			}
 
 			select {
 			case <-done:
+				log.Println("filterSpecificValues: stop filtering")
 				return
 			case filtered <- val:
+				log.Printf("filterSpecificValues: passed value %v\n", val)
 			}
 		}
 	}()
@@ -77,11 +86,14 @@ func bufferValues(done <-chan bool, data <-chan int) <-chan int {
 	b := NewRingBuffer(bufSize, bufEmitInterval)
 
 	go func() {
+		log.Println("bufferValues: start buffering")
 		for {
 			select {
 			case <-done:
+				log.Println("bufferValues: stop buffering")
 				return
 			case val := <-data:
+				log.Printf("bufferValues: buffering value %v\n", val)
 				b.Insert(NewValue(val))
 			}
 		}
@@ -91,6 +103,7 @@ func bufferValues(done <-chan bool, data <-chan int) <-chan int {
 		for {
 			select {
 			case <-done:
+				log.Println("bufferValues: stop emitting")
 				return
 			case <-time.After(b.emitTimer):
 				emitted := b.Emit()
@@ -98,8 +111,10 @@ func bufferValues(done <-chan bool, data <-chan int) <-chan int {
 					for _, data := range emitted {
 						select {
 						case <-done:
+							log.Println("bufferValues: stop emitting")
 							return
 						case buffered <- data.GetValue():
+							log.Printf("bufferValues: emitted value %v\n", data.GetValue())
 						}
 					}
 				}
